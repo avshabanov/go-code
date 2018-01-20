c9b13ae00e1ddda443373cf8e55c6077d9e18263
Alexander Shabanov <avshabanov@gmail.com>
2018-01-19 21:21:43 -0800

    Add independent / overlapped tx sample

--- db/bolt_tx/main.go
+++ db/bolt_tx/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"io/ioutil"
 	"log"
@@ -9,6 +10,9 @@ import (
 
 	"github.com/boltdb/bolt"
 )
+
+var helloBucketName = []byte("Hello")
+var keyOne = []byte{1}
 
 func disposeTempDB(db *bolt.DB) {
 	path := db.Path()
@@ -37,9 +41,7 @@ func tempfile() string {
 	return f.Name()
 }
 
-var helloBucketName = []byte("Hello")
-
-func tx(msg string, db *bolt.DB, q chan string) {
+func updateCounter(msg string, key []byte, db *bolt.DB, q chan string) {
 	err := db.Update(func(tx *bolt.Tx) error {
 		b := tx.Bucket(helloBucketName)
 		if b == nil {
@@ -48,9 +50,9 @@ func tx(msg string, db *bolt.DB, q chan string) {
 
 		time.Sleep(time.Second)
 
-		b.Get([]byte("1"))
+		val := b.Get(key)
 
-		if err := b.Put([]byte("1"), []byte("val-"+msg)); err != nil {
+		if err := b.Put(key, []byte(string(val)+":"+msg)); err != nil {
 			return err
 		}
 
@@ -67,22 +69,14 @@ func tx(msg string, db *bolt.DB, q chan string) {
 	q <- fmt.Sprintf("<done: %s>", msg)
 }
 
-func main() {
-	db, err := bolt.Open(tempfile(), 0664, nil)
-	if err != nil {
-		panic(err)
-	}
-	defer disposeTempDB(db)
-
-	fmt.Printf("Opened test DB: %s\n", db.Path())
-
+func demoOverlappingUpdates(db *bolt.DB) {
 	if err := db.Update(func(tx *bolt.Tx) error {
 		b, err := tx.CreateBucketIfNotExists(helloBucketName)
 		if err != nil {
 			return err
 		}
 
-		if err := b.Put([]byte("1"), []byte("initial")); err != nil {
+		if err := b.Put(keyOne, []byte("init-0")); err != nil {
 			return err
 		}
 
@@ -96,11 +90,90 @@ func main() {
 	execQueue := make(chan string, count)
 
 	for i := 0; i < count; i++ {
-		go tx(fmt.Sprintf("tx-%d", i+1), db, execQueue)
+		go updateCounter(fmt.Sprintf("tx-%d", i+1), keyOne, db, execQueue)
 	}
 
 	for i := 0; i < count; i++ {
 		msg := <-execQueue
 		fmt.Printf("msg %d = %s\n", i, msg)
+	}
+
+	// show result
+	db.View(func(tx *bolt.Tx) error {
+		b := tx.Bucket(helloBucketName)
+		if b == nil {
+			return fmt.Errorf("Hello bucket doesn't exist")
+		}
+
+		val := b.Get(keyOne)
+		log.Printf("End result: 1 => %s", string(val))
+
+		return nil
+	})
+}
+
+func demoIndependentUpdates(db *bolt.DB) {
+	if err := db.Update(func(tx *bolt.Tx) error {
+		b, err := tx.CreateBucketIfNotExists(helloBucketName)
+		if err != nil {
+			return err
+		}
+
+		if err := b.Put(keyOne, []byte("init-0")); err != nil {
+			return err
+		}
+
+		log.Println("inserted test data")
+		return nil
+	}); err != nil {
+		panic(err)
+	}
+
+	const count = 3
+	execQueue := make(chan string, count)
+
+	for i := 0; i < count; i++ {
+		go updateCounter(fmt.Sprintf("tx-%d", i+1), []byte{byte(i + 1)}, db, execQueue)
+	}
+
+	for i := 0; i < count; i++ {
+		msg := <-execQueue
+		fmt.Printf("msg %d = %s\n", i, msg)
+	}
+
+	// show result
+	db.View(func(tx *bolt.Tx) error {
+		b := tx.Bucket(helloBucketName)
+		if b == nil {
+			return fmt.Errorf("Hello bucket doesn't exist")
+		}
+
+		val := b.Get(keyOne)
+		log.Printf("End result: 1 => %s", string(val))
+
+		return nil
+	})
+}
+
+func main() {
+	mode := flag.String("mode", "overlapping-updates", "Specifies what demo to run")
+	flag.Parse()
+
+	db, err := bolt.Open(tempfile(), 0664, nil)
+	if err != nil {
+		panic(err)
+	}
+	defer disposeTempDB(db)
+
+	fmt.Printf("Opened test DB: %s\n", db.Path())
+
+	switch *mode {
+	case "overlapping-updates":
+		demoOverlappingUpdates(db)
+	case "independent-updates":
+		demoIndependentUpdates(db)
+	default:
+		fmt.Printf("Wrong mode: %s\n", *mode)
+		flag.Usage()
 	}
 }
